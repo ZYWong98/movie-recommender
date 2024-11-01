acbf4aa31e6a2bb3f9194831448321deab3256b5
A <63173933+axiom-code56@users.noreply.github.com>
2024-10-31 22:20:33 -0500

    adding imports and loading file

--- OverviewRecommendaiton.py
+++ OverviewRecommendaiton.py
@@ -1,1 +1,6 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
+# Load the CSV file
+df = pd.read_csv("movieData.csv")  # Replace with the path to your file
