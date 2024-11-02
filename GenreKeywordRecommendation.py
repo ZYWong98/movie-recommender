eb341308fdc874343a9971c8d0b55c3b396c5e62
A <63173933+axiom-code56@users.noreply.github.com>
2024-11-02 10:32:52 -0500

    import libraries and clean data

--- GenreKeywordRecommendation.py
+++ GenreKeywordRecommendation.py
@@ -1,1 +1,20 @@
 
+#Import Libraries
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+#Clean Data
+# Assuming df is your DataFrame and contains genre and keywords columns.
+# Remove stopwords, make words lowercase, and clean up genre and keywords.
+# Ensure both columns contain strings of words.
+def clean_data(x):
+    if isinstance(x, str):
+        return x.lower()
+    else:
+        return ''
+
+df['genre'] = df['genre'].apply(clean_data)
+df['keywords'] = df['keywords'].apply(clean_data)
+
