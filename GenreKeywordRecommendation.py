0c55c5edd458efeadcec3141f75e0cfae1c6daa0
A <63173933+axiom-code56@users.noreply.github.com>
2024-11-02 10:39:31 -0500

    Update GenreKeywordRecommendation.py

--- GenreKeywordRecommendation.py
+++ GenreKeywordRecommendation.py
@@ -1,3 +1,12 @@
+""" 
+Objective for genre+keyword recommendation: 
+    clean the data again of stopwords, make words lowercase(just in case)
+    create 'metadata soup' (combine both genre and keywords into string of words to feed vectorizer)
+    use CountVectorizer from scikit-learn to fit soup and obtain matrix
+    use cosine similarity function to calculate score (fit count matrix twice)
+    create reverse of indices and map movie titles (map title as index)
+    make recommendation function (same function as above but replace consine similarity matrix)
+"""
 
 #Import Libraries
 import pandas as pd
